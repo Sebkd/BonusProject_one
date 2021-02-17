ef819aaaafdc32056bf2d3533c27523115b2e254
Sebkd <sebkd@mail.ru>
2021-02-17 22:23:21 +0700

    code

--- task-Matplotlib.py
+++ task-Matplotlib.py
@@ -1,2 +1,9 @@
 # task-Matplotlib
 '''Знакомство и работа с библиотекой Matplotlib'''
+import matplotlib
+import matplotlib.pyplot as plt
+
+plt.plot([i for i in range(5)], [i for i in range(5)])
+plt.show()
+
+print(matplotlib.__version__)
