95a8a24408043eab1bd8b0adad23c31cf59cba04
Sebkd <sebkd@mail.ru>
2021-02-17 21:48:40 +0700

    code decorator

--- task-decorator.py
+++ task-decorator.py
@@ -2,14 +2,26 @@
 '''тест для понимания декораторов'''
 
 def simple_decor(func):
-    def wrapper():
-        print('Run function')
-        func()
+    def wrapper(arg):
+        print(f'Run function {str(func.__name__)}()')
+        func(arg)
         print('End function')
     return wrapper
 
 @simple_decor
-def test():
-    print('Test function')
+def sqrt(num):
+    print(f'Корень квадратный из {num} =  {num ** 0.5}')
 
-test()
+class Test():
+    def __init__(self, x_axis, y_axis):
+        self._x_axis = x_axis
+        self._y_axis = y_axis
+
+    @simple_decor
+    def modul_vector(self):
+        print(f'Модуль вектора равен = {(self._x_axis * 2 + self._y_axis * 2) ** 0.5}')
+    pass
+
+sqrt(5)
+vector = Test(12, 6)
+vector.modul_vector()
