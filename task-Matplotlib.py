d3b97c27d43e2eba21ae68893821463649f328e2
Sebkd <sebkd@mail.ru>
2021-02-23 20:12:08 +0700

    final matplotlib

--- task-Matplotlib.py
+++ task-Matplotlib.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter, AutoMinorLocator)
 import matplotlib.gridspec as gridspec
+import math
 # -----------------------------------------------------
 # plt.plot([i for i in range(5)], [i for i in range(5)])
 # plt.show()
@@ -167,72 +168,177 @@ import matplotlib.gridspec as gridspec
 # plt.legend(bbox_to_anchor=(1, 0.6), frameon = False) # Расположение легенды вне поля графика, нужно открыть 1 вар
 # plt.show()
 # -----------------------------------------------------------------------------------------------------------------
-'''
-Класс GridSpec, позволяет задавать геометрию сетки и расположение на
-ней полей с графиками. На первый взгляд может показаться, что работа
-с GridSpec довольно неудобна и требует написания лишнего кода, но,
-если требуется расположить поля с графиками нетривиальным образом,
-то этот инструмент становится незаменимым. Перед тем как работать с
-GridSpec импортируйте его
-
-Объект класса GridSpec, создается в строке:
-gridspec.GridSpec(ncols=2, nrows=1, figure=fg)
-В конструктор класса передается количество столбцов, строк и Фигура,
-на которой все будет отображено.
-Альтернативный вариант создания объекта GridSpec выглядит так:
-gs = fg.add_gridspec(1, 2)
-Здесь fg - это объект Figure, у которого есть метод add_gridspec(),
-позволяющий добавить на него сетку с заданными параметрами (в
-нашем случае одна строка и два столбца).
-'''
-x = [1, 2, 3, 4, 5]
-y1 = [9, 4, 2, 4, 9]
-y2 = [1, 7, 6, 3, 5]
-y3 = [-7, -4, 2, -4, -7]
-# ---- Свободная компоновка трех графиков
-# fg = plt.figure(figsize=(9, 4), constrained_layout=True)
-# gs = fg.add_gridspec(2, 2)
-# fig_ax_1 = fg.add_subplot(gs[0, :])
-# plt.plot(x, y2)
-# fig_ax_2 = fg.add_subplot(gs[1, 0])
-# plt.plot(x, y1)
-# fig_ax_3 = fg.add_subplot(gs[1, 1])
-# plt.plot(x, y3)
-# ---- Свободная компановка без графиков
-# fg = plt.figure(figsize=(9, 9), constrained_layout=True)
-# gs = fg.add_gridspec(5, 5)
-# fig_ax_1 = fg.add_subplot(gs[0, :3])
-# fig_ax_1.set_title('gs[0, :3]')
-# fig_ax_2 = fg.add_subplot(gs[0, 3:])
-# fig_ax_2.set_title('gs[0, 3:]')
-# fig_ax_3 = fg.add_subplot(gs[1:, 0])
-# fig_ax_3.set_title('gs[1:, 0]')
-# fig_ax_4 = fg.add_subplot(gs[1:, 1])
-# fig_ax_4.set_title('gs[1:, 1]')
-# fig_ax_5 = fg.add_subplot(gs[1, 2:])
-# fig_ax_5.set_title('gs[1, 2:]')
-# fig_ax_6 = fg.add_subplot(gs[2:4, 2])
-# fig_ax_6.set_title('gs[2:4, 2]')
-# fig_ax_7 = fg.add_subplot(gs[2:4, 3:])
-# fig_ax_7.set_title('gs[2:4, 3:]')
-# fig_ax_8 = fg.add_subplot(gs[4, 3:])
-# fig_ax_8.set_title('gs[4, 3:]')
-# ------ Можно заранее задать размеры областей и передать их в качестве параметров в виде массивов:
-fg = plt.figure(figsize=(5, 5),constrained_layout=True)
-widths = [1, 3]
-heights = [2, 0.7]
-gs = fg.add_gridspec(ncols=2, nrows=2, width_ratios=widths,
-height_ratios=heights)
-fig_ax_1 = fg.add_subplot(gs[0, 0])
-fig_ax_1.set_title('w:1, h:2')
-fig_ax_2 = fg.add_subplot(gs[0, 1])
-fig_ax_2.set_title('w:3, h:2')
-fig_ax_3 = fg.add_subplot(gs[1, 0])
-fig_ax_3.set_title('w:1, h:0.7')
-fig_ax_4 = fg.add_subplot(gs[1, 1])
-fig_ax_4.set_title('w:3, h:0.7')
-plt.show()
+# '''
+# Класс GridSpec, позволяет задавать геометрию сетки и расположение на
+# ней полей с графиками. На первый взгляд может показаться, что работа
+# с GridSpec довольно неудобна и требует написания лишнего кода, но,
+# если требуется расположить поля с графиками нетривиальным образом,
+# то этот инструмент становится незаменимым. Перед тем как работать с
+# GridSpec импортируйте его
+#
+# Объект класса GridSpec, создается в строке:
+# gridspec.GridSpec(ncols=2, nrows=1, figure=fg)
+# В конструктор класса передается количество столбцов, строк и Фигура,
+# на которой все будет отображено.
+# Альтернативный вариант создания объекта GridSpec выглядит так:
+# gs = fg.add_gridspec(1, 2)
+# Здесь fg - это объект Figure, у которого есть метод add_gridspec(),
+# позволяющий добавить на него сетку с заданными параметрами (в
+# нашем случае одна строка и два столбца).
+# '''
+# x = [1, 2, 3, 4, 5]
+# y1 = [9, 4, 2, 4, 9]
+# y2 = [1, 7, 6, 3, 5]
+# y3 = [-7, -4, 2, -4, -7]
+# # ---- Свободная компоновка трех графиков
+# # fg = plt.figure(figsize=(9, 4), constrained_layout=True)
+# # gs = fg.add_gridspec(2, 2)
+# # fig_ax_1 = fg.add_subplot(gs[0, :])
+# # plt.plot(x, y2)
+# # fig_ax_2 = fg.add_subplot(gs[1, 0])
+# # plt.plot(x, y1)
+# # fig_ax_3 = fg.add_subplot(gs[1, 1])
+# # plt.plot(x, y3)
+# # ---- Свободная компановка без графиков
+# # fg = plt.figure(figsize=(9, 9), constrained_layout=True)
+# # gs = fg.add_gridspec(5, 5)
+# # fig_ax_1 = fg.add_subplot(gs[0, :3])
+# # fig_ax_1.set_title('gs[0, :3]')
+# # fig_ax_2 = fg.add_subplot(gs[0, 3:])
+# # fig_ax_2.set_title('gs[0, 3:]')
+# # fig_ax_3 = fg.add_subplot(gs[1:, 0])
+# # fig_ax_3.set_title('gs[1:, 0]')
+# # fig_ax_4 = fg.add_subplot(gs[1:, 1])
+# # fig_ax_4.set_title('gs[1:, 1]')
+# # fig_ax_5 = fg.add_subplot(gs[1, 2:])
+# # fig_ax_5.set_title('gs[1, 2:]')
+# # fig_ax_6 = fg.add_subplot(gs[2:4, 2])
+# # fig_ax_6.set_title('gs[2:4, 2]')
+# # fig_ax_7 = fg.add_subplot(gs[2:4, 3:])
+# # fig_ax_7.set_title('gs[2:4, 3:]')
+# # fig_ax_8 = fg.add_subplot(gs[4, 3:])
+# # fig_ax_8.set_title('gs[4, 3:]')
+# # ------ Можно заранее задать размеры областей и передать их в качестве параметров в виде массивов:
+# fg = plt.figure(figsize=(5, 5),constrained_layout=True)
+# widths = [1, 3]
+# heights = [2, 0.7]
+# gs = fg.add_gridspec(ncols=2, nrows=2, width_ratios=widths,
+# height_ratios=heights)
+# fig_ax_1 = fg.add_subplot(gs[0, 0])
+# fig_ax_1.set_title('w:1, h:2')
+# fig_ax_2 = fg.add_subplot(gs[0, 1])
+# fig_ax_2.set_title('w:3, h:2')
+# fig_ax_3 = fg.add_subplot(gs[1, 0])
+# fig_ax_3.set_title('w:1, h:0.7')
+# fig_ax_4 = fg.add_subplot(gs[1, 1])
+# fig_ax_4.set_title('w:3, h:0.7')
+# plt.show()
 # ---------------------------------------------------------------------------------------------------------------
+# '''Работа с текстом
+# В части текстового наполнения при построении графика выделяют
+# следующие составляющие:
+# • заголовок поля (title);
+# • заголовок фигуры (suptitle);
+# • подписи осей (xlabel, ylabel);
+# • тестовый блок на поле графика (text), либо на фигуре (figtext);
+# • аннотация (annotate) - текст и указатель.
+# '''
+# plt.figure(figsize=(10,4))
+# plt.figtext(0.5, -0.1, 'figtext')
+# plt.suptitle('suptitle')
+# plt.subplot(121)
+# plt.title('title')
+# plt.xlabel('xlabel')
+# plt.ylabel('ylabel')
+# plt.text(0.2, 0.2, 'text')
+# plt.annotate('annotate', xy=(0.2, 0.4), xytext=(0.6, 0.7),
+#              arrowprops=dict(facecolor='black', shrink=0.05))
+# plt.subplot(122)
+# plt.title('title')
+# plt.xlabel('xlabel')
+# plt.ylabel('ylabel')
+# plt.text(0.5, 0.5, 'text')
+# ---------------------------
+# ''' Функция title() также поддерживает в качестве аргументов свойства класса Text: '''
+# weight=['light', 'regular', 'bold']
+# plt.figure(figsize=(12, 4))
+# for i, lc in enumerate(['left', 'center', 'right']):
+#     plt.subplot(1, 3, i+1)
+#     plt.title(label=lc, loc=lc, fontsize=12+i*5, fontweight=weight[i], pad=10+i*15)
+# plt.show()
+# ---------------------------
+# '''При работе с pyplot, для установки подписей осей графика
+# используются функции labelx() и labely(), при работе с объектом
+# Axes - функции set_xlabel() и set_ylabel().'''
+# # x = [i for i in range(10)]
+# # y = [i*2 for i in range(10)]
+# # plt.plot(x, y)
+# # plt.xlabel('Ось X')
+# # plt.ylabel('Ось Y')
+# plt.xlabel('Ось X\nНезависимая величина', fontsize=14, fontweight='bold')
+# plt.ylabel('Ось Y\nЗависимая величина', fontsize=14, fontweight='bold')
+# # plt.text(0, 7, 'HELLO!', fontsize=15)
+# # plt.plot(range(0,10), range(0,10))
+# bbox_properties=dict(boxstyle='darrow, pad=0.3', ec='k', fc='y', ls='-', lw=3)
+# plt.text(2, 7, 'HELLO!', fontsize=15, bbox=bbox_properties)
+# plt.plot(range(0,10), range(0,10))
+# plt.show()
+# -----------------------------------------
+''' Инструмент Аннотация позволяет установить текстовый блок с заданным
+содержанием и стрелкой для указания на конкретное место на графике.
+Для создания аннотации используется функция annotate(). основными
+ее аргументами являются:
+• text: str
+◦ Текст аннотации.
+• xy: (float, float)
+◦ Координаты места, на которое будет указывать стрелка.
+• xytext: (float, float), optional
+◦ Координаты расположения текстовой надписи.
+• xycoords: str
+◦ Система координат, в которой определяется расположение
+указателя.
+• textcoords: str
+◦ Система координат, в которой определяется расположение
+текстового блока.
+• arrowprops: dict, optional
+◦ Параметры отображения стрелки. Имена этих параметров
+(ключи словаря) являются параметрами конструктора объекта
+класса FancyArrowPatch.'''
+
+# x = list(range(-5, 6))
+# y = [i**2 for i in x]
+# plt.annotate('min', xy=(0, 0), xycoords='data', xytext=(0, 10), textcoords='data', arrowprops=dict(facecolor='g'))
+# plt.plot(x, y)
+# plt.show()
+# '''Стили аннотаций стрелок'''
+# plt.figure(figsize=(7,5))
+# arrows = ['-', '->', '-[', '|-|', '-|>', '<-', '<->', '<|-', '<|-|>', 'fancy', 'simple', 'wedge']
+# bbox_properties=dict( boxstyle='round,pad=0.2',
+#                       ec='k',
+#                       fc='w',
+#                       ls='-',
+#                       lw=1 )
+# ofs_x = 0
+# ofs_y = 0
+# for i, ar in enumerate(arrows):
+#     if i == 6: ofs_x = 0.5
+#     plt.annotate(ar, xy=(0.4+ofs_x, 0.92-ofs_y), xycoords='data',
+#                  xytext=(0.05+ofs_x, 0.9-ofs_y), textcoords='data', fontsize=17,
+#                  bbox=bbox_properties, arrowprops=dict(arrowstyle=ar))
+#     if ofs_y == 0.75: ofs_y = 0
+#     else: ofs_y += 0.15
+# plt.show()
+# -------------------------------------------------------------------------------------------
+'''Заливка области между графиком и осью'''
+
+x = np.arange(0.0, 5, 0.01)
+y = np.cos(x*np.pi)
+plt.plot(x, y, c = 'r')
+plt.fill_between(x, y)
+plt.show()
+
+
+
 
 
 
